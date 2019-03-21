99e88d7d828927dc9696be83cfca35523e110319
YCKJ1697 <sunzhengjie@cloudwalk.cn>
2019-03-21 20:43:01 +0800

    javaagent

---
+++ agent/src/main/java/TestAgent.java
@@ -0,0 +1,10 @@
+public class TestAgent {  
+    public static void main(String[] args) {  
+        TestAgent ta = new TestAgent();  
+        ta.test();  
+    }  
+    
+    public void test() {  
+         System.out.println("I'm TestAgent");  
+    }  
+}  
